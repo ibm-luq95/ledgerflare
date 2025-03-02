6ebc945c97a57f166765caae6b9eef6a3d66206b
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-03-03 00:31:56 +0300

    No changes

--- src/job/filters/job.py
+++ src/job/filters/job.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-#
 import django_filters
 from django import forms
+from django.db.models.sql.where import WhereNode
 
 from core.filters.filter_help_text import HelpfulFilterSet
 from core.utils.developments.debugging_print_object import DebuggingPrint
@@ -10,6 +11,12 @@ from django.utils.translation import gettext as _
 
 
 class JobFilter(HelpfulFilterSet):
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     # DebuggingPrint.dir(self.form)
+    #     DebuggingPrint.pprint(self.form.fields.get("show_all"))
+
     due_date = django_filters.DateFilter(
         field_name="due_date", widget=forms.DateInput(attrs={"type": "date"})
     )
@@ -30,31 +37,41 @@ class JobFilter(HelpfulFilterSet):
         widget=forms.SelectMultiple(attrs={"data_name": "job-categories"}),
         lookup_expr="exact",
     )
-    show_all = django_filters.BooleanFilter(
-        label=_("Show all jobs"),
-        method="filter_show_all",
-        help_text=_("Check this box to show all jobs."),
-    )
+    # show_all = django_filters.ChoiceFilter(
+    #     label=_("Show all jobs"),
+    #     method="filter_show_all",
+    #     help_text=_("Check this box to show all jobs."),
+    #     choices=[
+    #         ("true", _("Yes")),
+    #     ],
+    #     empty_label=_("Select an option"),
+    # )
 
-    def filter_show_all(self, queryset, name, value):
-        """Custom filter to return all jobs if 'show_all' is checked."""
-        if value:  # If the checkbox is checked (True)
-            return JobProxy.original_objects.all()  # Return all jobs
-        return queryset  # Otherwise, return the filtered queryset
+    # def filter_show_all(self, queryset, name, value):
+    #     if value:
+    #         # Capture existing filters
+    #         where_clause: WhereNode = queryset.query.where
+    #         DebuggingPrint.print(where_clause)
+    #         # DebuggingPrint.inspect(where_clause, is_all=True)
+    #         # DebuggingPrint.print(type(where_clause))
+    #         DebuggingPrint.pprint(where_clause.get_source_expressions())
+    #
+    #         # Create new queryset with original manager
+    #         new_qs = JobProxy.original_objects.all()
+    #
+    #         # Apply captured filters to new manager
+    #         return new_qs.filter(where_clause)
+    #
+    #     return queryset
 
     class Meta:
         model = JobProxy
-        # form = JobFilterForm
         fields = {
             "title": ["icontains"],
             "managed_by": ["exact"],
             "period_year": ["exact"],
             "period_month": ["exact"],
-            # "bookkeeper": ["icontains"],
             "client": ["exact"],
-            # "job_type": ["exact"],
             "status": ["exact"],
             "state": ["exact"],
-            # "categories": ["exact"],
-            # "due_date": ["gt", "lt"],
         }
