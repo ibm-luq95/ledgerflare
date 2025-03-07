224252cd844c57596f49ff307bff6a3bf9905e8f
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-03-08 01:08:15 +0300

    new: add discussion-notifications api rul

--- src/discussion/urls/api.py
+++ src/discussion/urls/api.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-#
+from django.urls import path
 from rest_framework import routers
 
 from discussion.views import DiscussionViewSet
+from discussion.views.api import DiscussionNotificationsApiView
 
 app_name = "api"
 
 router = routers.DefaultRouter()
 router.register(r"discussion-api", DiscussionViewSet, basename="discussion-api-router")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        "discussion-notifications",
+        DiscussionNotificationsApiView.as_view(),
+        name="set-discussion-notifications",
+    ),
+] + router.urls
