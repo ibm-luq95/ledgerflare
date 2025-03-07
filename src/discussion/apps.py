d403b852b343b6e20ce35cdc3efd24ae450a4bde
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-03-08 01:09:18 +0300

    new: import new signal

--- src/discussion/apps.py
+++ src/discussion/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DiscussionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "discussion"
+
+    def ready(self):
+        import discussion.signals.notification  # noqa
