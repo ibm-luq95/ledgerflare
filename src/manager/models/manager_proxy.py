36e91b5d02fb54957aee11ec8cf894cd23661c08
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-04-29 19:51:04 +0300

    feat: implement soft delete method in ManagerProxy with custom signal triggers

--- src/manager/models/manager_proxy.py
+++ src/manager/models/manager_proxy.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-#
-from .manager import Manager
+from django.utils import timezone
+
+from manager.models import Manager
+from manager.signals.signals import (
+    manager_pre_soft_delete,
+    manager_post_soft_delete,
+)
 
 
 class ManagerProxy(Manager):
     class Meta(Manager.Meta):
         proxy = True
+
+    def delete(self):
+        """
+        Perform a soft delete and trigger custom signals.
+        """
+        # Trigger pre_soft_delete signal
+        manager_pre_soft_delete.send(sender=self.__class__, instance=self)
+
+        # Perform soft delete
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+        # Trigger post_soft_delete signal
+        manager_post_soft_delete.send(sender=self.__class__, instance=self)
