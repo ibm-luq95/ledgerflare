de1f72feb8745081de8c8b47b1c025f5d4d980a8
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-04-29 15:23:56 +0300

    feat: add ready method to BookkeeperConfig for signal handlers import

--- src/bookkeeper/apps.py
+++ src/bookkeeper/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BookkeeperConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bookkeeper"
+
+    def ready(self):
+        import bookkeeper.signals.handlers  # noqa: F401
