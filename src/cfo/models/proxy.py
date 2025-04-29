3ed1ae0629a6f25b2fcfef4119e1bec5ce3a3839
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-04-29 19:49:00 +0300

    feat: implement soft delete functionality in CFOProxy with custom signal triggers

--- src/cfo/models/proxy.py
+++ src/cfo/models/proxy.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-#
+from django.utils import timezone
+
 from cfo.models.cfo import CFO
+from cfo.signals.signals import cfo_pre_soft_delete, cfo_post_soft_delete
 
 
 class CFOProxy(CFO):
 
     class Meta(CFO.Meta):
         proxy = True
+
+    def delete(self):
+        """
+        Perform a soft delete and trigger custom signals.
+        """
+        # Trigger pre_soft_delete signal
+        cfo_pre_soft_delete.send(sender=self.__class__, instance=self)
+
+        # Perform soft delete
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+        # Trigger post_soft_delete signal
+        cfo_post_soft_delete.send(sender=self.__class__, instance=self)
