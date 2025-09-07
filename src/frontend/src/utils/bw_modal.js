13af7e821d0acfc677496719b077b300ad707872
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-09-07 18:38:37 +0300

    no changes

--- src/frontend/src/utils/bw_modal.js
+++ src/frontend/src/utils/bw_modal.js
@@ -5,8 +5,6 @@
  * @param {string} msg the message in the notification
  * @param {string} notificationType notification message type
  */
-const BWJSModal = (msg, notificationType = "success") => {
-  
-};
+const BWJSModal = (msg, notificationType = "success") => {};
 
 export { BWJSModal };
