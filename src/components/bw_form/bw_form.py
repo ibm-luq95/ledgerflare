853108429baacd0dc69fa2ed390f582a6f25f2b2
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-10-28 20:43:58 +0300

    fix: disable cache

--- src/components/bw_form/bw_form.py
+++ src/components/bw_form/bw_form.py
@@ -13,6 +13,9 @@ class BWForm(component.Component):
     js_file = "bw_form/bw_form.js"
     css_file = "bw_form/bw_form.css"
 
+    class Cache:
+        enabled = False
+
     # This component takes one parameter, a date string to show in the template
     def get_context_data(
         self,
