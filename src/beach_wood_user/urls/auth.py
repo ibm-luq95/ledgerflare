872e153aafc14a4b2c5a58a799905b90b83f90b1
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-10-28 20:40:22 +0300

    dev: organize import

--- src/beach_wood_user/urls/auth.py
+++ src/beach_wood_user/urls/auth.py
@@ -2,7 +2,10 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from beach_wood_user.views import BWLoginViewBW, BWForgetPasswordView, BWLogoutView
+from beach_wood_user.views import BWForgetPasswordView
+from beach_wood_user.views import BWLoginViewBW
+from beach_wood_user.views import BWLogoutView
+
 
 app_name = "auth"
 
