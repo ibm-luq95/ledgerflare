db1d28e10b226217060b36920b390abd1f323959
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-01-24 22:55:54 +0300

    fix: fix mount element

--- src/frontend/src/application/app_vue.js
+++ src/frontend/src/application/app_vue.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "../components/App.vue";
-
-createApp(App).mount("#app");
+if (document.querySelector("#app")) {
+  createApp(App).mount("#app");
+}
